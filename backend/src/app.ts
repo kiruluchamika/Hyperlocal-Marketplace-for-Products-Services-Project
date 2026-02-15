0eeccf39e1bd3feb8a3d3ac2e71489dfac3ecc40
Kirulu chamika <kiruluchamika2002@gmail.com>
2026-02-15 17:07:24 +0530

    Setup The Swagger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 import routes from "./routes";
 import { notFound } from "./middlewares/notFound";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -8,6 +10,35 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const swaggerSpec = swaggerJsdoc({
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "LocalLink API",
+			version: "0.1.0",
+			description: "API documentation for LocalLink backend",
+		},
+		components: {
+			securitySchemes: {
+				bearerAuth: {
+					type: "http",
+					scheme: "bearer",
+					bearerFormat: "JWT",
+				},
+			},
+		},
+		servers: [{ url: "/api" }],
+	},
+	apis: [
+		"./src/routes/*.ts",
+		"./src/controllers/*.ts",
+		"./dist/routes/*.js",
+		"./dist/controllers/*.js",
+	],
+});
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api", routes);
 
