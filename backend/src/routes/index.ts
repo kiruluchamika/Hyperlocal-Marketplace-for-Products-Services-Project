a5940196d451aab928c28595652b5eba7ade23ee
Gamindu Kalmadu <gamindukalmadu8@gmail.com>
2026-02-18 03:41:05 +0530

    add category routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRoutes from "./authRoutes";
 import userRoutes from "./userRoutes";
 import listingRoutes from "./listingRoutes";
+import categoryRoutes from "./categoryRoutes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/listings", listingRoutes);
+router.use("/categories", categoryRoutes);
 
 export default router;
